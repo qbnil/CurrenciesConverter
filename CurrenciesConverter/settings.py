475889095c105043811472fbda81e834437b1cc0
qbnil <130576530+qbnil@users.noreply.github.com>
2023-06-23 10:25:37 +0300

    Updated settings.py

--- CurrenciesConverter/settings.py
+++ CurrenciesConverter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9&081na!jc*cyy$_)x(vxeme-k_xi_2ibqjia^ddq+j8l7+dho'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
